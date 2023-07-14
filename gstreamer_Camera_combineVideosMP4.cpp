e2c2cace35ed008b7b81eb29fb617529d16b00d0
mmrsaeed <mmrsaeed@syngenta.com>
2023-07-14 18:10:20 +0100

    new3

--- gstreamer_Camera_combineVideosMP4.cpp
+++ gstreamer_Camera_combineVideosMP4.cpp
@@ -2,49 +2,43 @@
 
 //g++ -o my_program my_program.cpp `pkg-config --cflags --libs gstreamer-1.0`
 
+int main(int argc, char *argv[]) {
+  gst_init(&argc, &argv);
 
-int main() {
-  // Initialize GStreamer.
-  gst_init(NULL, NULL);
-
-  // Create a pipeline.
-  gst_element_factory_set_data(
-      gst_element_factory_find("videotestsrc"), "num-buffers", G_TYPE_INT, 10);
   GstElement *pipeline = gst_pipeline_new("pipeline");
 
-  // Create the video sources.
-  GstElement *source1 = gst_element_factory_create("v4l2src", "source1");
-  GstElement *source2 = gst_element_factory_create("v4l2src", "source2");
+  GstElement *source1 = gst_element_factory_make("v4l2src", "source1");
+  g_object_set(source1, "device", "/dev/video0", NULL);
+  GstElement *encoder1 = gst_element_factory_make("x264enc", "encoder1");
+  GstElement *parser1 = gst_element_factory_make("h264parse", "parser1");
 
-  // Set the properties of the video sources.
-  gst_element_set_property(source1, "device", "/dev/video0");
-  gst_element_set_property(source2, "device", "/dev/video1");
+  GstElement *source2 = gst_element_factory_make("v4l2src", "source2");
+  g_object_set(source2, "device", "/dev/video2", NULL);
+  GstElement *encoder2 = gst_element_factory_make("x264enc", "encoder2");
+  GstElement *parser2 = gst_element_factory_make("h264parse", "parser2");
 
-  // Add the video sources to the pipeline.
-  gst_bin_add_many(GST_BIN(pipeline), source1, source2, NULL);
+  GstElement *mux = gst_element_factory_make("matroskamux", "mux");
+  GstElement *sink = gst_element_factory_make("filesink", "sink");
+  g_object_set(sink, "location", "output.mkv", NULL);
 
-  // Create the video sink.
-  GstElement *sink = gst_element_factory_create("filesink", "sink");
+  gst_bin_add_many(GST_BIN(pipeline), source1, encoder1, parser1, source2, encoder2, parser2, mux, sink, NULL);
+  gst_element_link_many(source1, encoder1, parser1, mux, NULL);
+  gst_element_link_many(source2, encoder2, parser2, mux, NULL);
+  gst_element_link(mux, sink);
 
-  // Set the properties of the video sink.
-  gst_element_set_property(sink, "location", "output.mp4");
-
-  // Add the video sink to the pipeline.
-  gst_bin_add(GST_BIN(pipeline), sink);
-
-  // Link the elements in the pipeline.
-  gst_element_link_many(source1, source2, sink, NULL);
-
-  // Start the pipeline.
   gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
-  // Wait for the pipeline to finish.
-  gst_element_wait_until_state_changed(pipeline, GST_STATE_PLAYING, GST_STATE_PAUSED);
+  GstBus *bus = gst_element_get_bus(pipeline);
+GstMessage *msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, 
+    static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
-  // Cleanup.
+
+  if (msg != NULL) gst_message_unref(msg);
+  gst_object_unref(bus);
   gst_element_set_state(pipeline, GST_STATE_NULL);
   gst_object_unref(pipeline);
 
   return 0;
 }
+
 
