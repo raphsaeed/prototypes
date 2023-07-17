06ce5dd9b0384762b7a1c8548aceb1fa35083b37
raphaelsaeed <raphael.saeed@syngenta.com>
2023-07-17 12:53:56 +0100

    new

--- gstreamer_Camera_jetson_captureImages.cpp
+++ gstreamer_Camera_jetson_captureImages.cpp
@@ -1,13 +1,20 @@
 #include </usr/include/gstreamer-1.0/gst/gst.h>
 #include <stdio.h>
+#include <string>
+
 //> g++ -o capture_images capture_images.cpp $(pkg-config --cflags --libs gstreamer-1.0)
 
 // Number of images to capture from each camera
 const int NUM_IMAGES = 10;
 
+// Folder to save the captured images
+const std::string SAVE_FOLDER = "/path/to/save/folder/";
+
 // Callback function to handle new video frames
 static GstFlowReturn new_sample_cb(GstElement* pipeline, gpointer user_data)
 {
+    static int count = 0;
+
     GstSample* sample;
     g_signal_emit_by_name(pipeline, "pull-sample", &sample);
     
@@ -16,9 +23,19 @@ static GstFlowReturn new_sample_cb(GstElement* pipeline, gpointer user_data)
         GstBuffer* buffer = gst_sample_get_buffer(sample);
         GstMapInfo map;
         gst_buffer_map(buffer, &map, GST_MAP_READ);
+
+        std::string filename = SAVE_FOLDER + "image_" + std::to_string(count) + ".jpeg";
         
-        // TODO: Save the image to file or perform further processing
-        
+        // Save the image to file
+        FILE* file = fopen(filename.c_str(), "wb");
+        if (file)
+        {
+            fwrite(map.data, 1, map.size, file);
+            fclose(file);
+        }
+
+        count++;
+
         gst_buffer_unmap(buffer, &map);
         gst_sample_unref(sample);
     }
@@ -48,41 +65,12 @@ int main(int argc, char* argv[])
     gst_element_set_state(pipeline2, GST_STATE_PLAYING);
     
     // Wait for the specified number of images from each camera
-    int count1 = 0;
-    int count2 = 0;
-    
-    while (count1 < NUM_IMAGES && count2 < NUM_IMAGES)
+    while (true)
     {
-        GstMessage* msg = gst_bus_timed_pop_filtered(gst_element_get_bus(pipeline1), GST_CLOCK_TIME_NONE,
-                                                     GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
-        
-        if (msg != NULL)
-        {
-            GError* err;
-            gchar* debug_info;
-            
-            switch (GST_MESSAGE_TYPE(msg))
-            {
-                case GST_MESSAGE_ERROR:
-                    gst_message_parse_error(msg, &err, &debug_info);
-                    g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
-                    g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
-                    g_clear_error(&err);
-                    g_free(debug_info);
-                    break;
-                case GST_MESSAGE_EOS:
-                    g_print("End-Of-Stream reached.\n");
-                    break;
-                default:
-                    g_printerr("Unexpected message received.\n");
-                    break;
-            }
-            
-            gst_message_unref(msg);
-            break;
-        }
-        
         g_main_context_iteration(NULL, FALSE);
+
+        if (count1 >= NUM_IMAGES && count2 >= NUM_IMAGES)
+            break;
     }
     
     // Stop the pipelines
