9b2c175e127fc7104e61a84a7e8576902e605829
raphaelsaeed <raphael.saeed@syngenta.com>
2023-07-17 16:21:10 +0100

    push

--- gstreamer_Camera_combineVideos.cpp
+++ gstreamer_Camera_combineVideos.cpp
@@ -1,58 +1,71 @@
+
+//>  g++ -o combineVideos gstreamer_Camera_combineVideos.cpp $(pkg-config --cflags --libs gstreamer-1.0)
 #include </usr/include/gstreamer-1.0/gst/gst.h>
-
-//g++ -o my_program my_program.cpp `pkg-config --cflags --libs gstreamer-1.0`
-
+#include <string>
 
 int main(int argc, char *argv[]) {
-    // Initialize GStreamer.
+    GstElement *pipeline;
+    GstBus *bus;
+    GstMessage *msg;
+    GstStateChangeReturn ret;
+
+    /* Initialize GStreamer */
     gst_init(&argc, &argv);
 
-    // Create a pipeline.
-    GstElement *pipeline = gst_pipeline_new("pipeline");
+    /* Create the pipeline */
+    std::string pipeline_desc = 
+      "v4l2src device=/dev/video0 ! videoconvert ! videoscale ! video/x-raw,width=640,height=480 ! queue ! videobox border-alpha=0 top=0 left=-640 ! videomixer name=mix ! "
+      "x264enc ! mp4mux ! filesink location=video.mp4 "
+      "v4l2src device=/dev/video2 ! videoconvert ! videoscale ! video/x-raw,width=640,height=480 ! queue ! videobox border-alpha=0 top=0 ! mix.";
+    pipeline = gst_parse_launch(pipeline_desc.c_str(), NULL);
 
-    // Create two video sources.
-    GstElement *source1 = gst_element_factory_make("v4l2src", "source1");
-    GstElement *source2 = gst_element_factory_make("v4l2src", "source2");
-
-    // Set the device names for the video sources.
-    g_object_set(source1, "device", "/dev/video0", NULL);
-    g_object_set(source2, "device", "/dev/video2", NULL);
-
-    // Create video converters
-    GstElement *convert1 = gst_element_factory_make("videoconvert", "convert1");
-    GstElement *convert2 = gst_element_factory_make("videoconvert", "convert2");
-
-    // Create a muxer to combine the video streams.
-    GstElement *muxer = gst_element_factory_make("flvmux", "muxer");
-
-    // Create a sink to write the video to a file.
-    GstElement *sink = gst_element_factory_make("filesink", "sink");
-
-    // Set the output filename for the sink.
-    g_object_set(sink, "location", "output.flv", NULL);
-
-    // Add the elements to the pipeline.
-    gst_bin_add_many(GST_BIN(pipeline), source1, convert1, source2, convert2, muxer, sink, NULL);
-
-    // Link the elements together.
-    if (!gst_element_link_many(source1, convert1, muxer, NULL) || !gst_element_link_many(source2, convert2, muxer, NULL) || !gst_element_link(muxer, sink)) {
-        g_warning("Failed to link elements!");
+    if (!pipeline) {
+        g_printerr("The pipeline could not be created.\n");
+        return -1;
     }
 
-    // Start the pipeline.
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    /* Start playing */
+    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr("Unable to set the pipeline to the playing state.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
 
-    // Wait for EOS or ERROR
-    GstBus *bus = gst_element_get_bus(pipeline);
-    GstMessage *msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+    /* Wait until error or EOS */
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
-    // Free resources
-    if (msg != NULL)
+    /* Parse message */
+    if (msg != NULL) {
+        GError *err;
+        gchar *debug_info;
+
+        switch (GST_MESSAGE_TYPE(msg)) {
+            case GST_MESSAGE_ERROR:
+                gst_message_parse_error(msg, &err, &debug_info);
+                g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
+                g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
+                g_clear_error(&err);
+                g_free(debug_info);
+                break;
+            case GST_MESSAGE_EOS:
+                g_print("End-Of-Stream reached.\n");
+                break;
+            default:
+                /* We should not reach here because we only asked for ERRORs and EOS */
+                g_printerr("Unexpected message received.\n");
+                break;
+        }
         gst_message_unref(msg);
+    }
+
+    /* Free resources */
     gst_object_unref(bus);
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(pipeline);
 
     return 0;
 }
+
 
