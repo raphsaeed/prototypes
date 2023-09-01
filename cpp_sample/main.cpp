a8c896e26a63a77cf009870a446f0b01b669d358
raphaelsaeed <raphael.saeed@syngenta.com>
2023-09-01 15:24:04 +0100

    code

---
+++ cpp_sample/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    const char* expectedOutput = "Hello Raphael";
+    std::cout << expectedOutput << std::endl;
+    
+    return 0;
+}
